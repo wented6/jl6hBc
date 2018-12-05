10866e4821b1825dabf4ac70c65cc4b10f753c63
wented6 <44713416+wented6@users.noreply.github.com>
2018-12-05 23:33:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,4 +117,22 @@ msg.delete();
 
 
 
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Diamond Codes`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Ween, ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`4season Brodcast`,"https://www.twitch.tv/xkilleryt")
+client.user.setStatus("online")
+ 
+});
+
+
+
+
 client.login(process.env.BOT_TOKEN);
