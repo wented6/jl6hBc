ec24fa9eb5f7653d5450966428b5deb1d4767eb8
wented6 <44713416+wented6@users.noreply.github.com>
2018-12-06 00:33:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -64,17 +64,31 @@ client.on('ready', () => {
 
 
 
- client.on('message', message => {
-            if(!message.channel.guild) return;
+client.on('message', message => {
+if (message.author.id === client.user.id) return;
+if (message.guild) {
+let embed = new Discord.RichEmbed()
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('#bc')){
- if(!message.author.id === '400692843582062604') return;
-message.channel.sendMessage('جار ارسال الرسالة');
-message.channel.sendMessage(`[ ${client.users.size} / ${client.users.filter(m => m.presence.status !== 'online').size} ] عدد المستلمين `);
-
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
+if(message.content.split(' ')[0] == prefix + 'bc') {
+if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
+if (!args[1]) {
+return;
+}
+  message.guild.members.forEach(m => {
+if(!message.member.hasPermission('ADMINISTRATOR')) return;
+      var bc = new Discord.RichEmbed()
+      .addField('# | الرسالة ', args)
+      .setThumbnail(message.guild.iconURL)
+      .setColor('RANDOM')
+      m.sendMessage(args)
+  });
+         if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply(":x: **ليس لديك صلاحية للنشر هنا**");
+  const AziRo = new Discord.RichEmbed()   
+  .setColor('RANDOM')
+  message.channel.sendEmbed(AziRo);          
+}
+} else {
+  return;
 }
 });
 
